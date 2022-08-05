b47576d2b1343f54d70b977f38058d07ecc20efc
Ax <blog@biuio.com>
2022-08-05 14:41:06 +0800

    更新各类电能获取方式

--- src/autoload.php
+++ src/autoload.php
@@ -2,7 +2,7 @@
 
 
 spl_autoload_register(function ($class) {
-    $file = (__DIR__) . '/' . substr(str_replace('\\', '/', $class), 6) . '.php';
+    $file = (__DIR__) . '/' . substr(str_replace('\\', '/', $class), 12) . '.php';
     if (!file_exists($file)) {
         throw new Exception('类文件不存在：' . $file);
     }
--- src/test/test.php
+++ src/test/test.php
@@ -14,38 +14,37 @@ class test
     public $dlt645;
     public function __construct()
     {
-        $this->ttt();
-        $this->conn =  new conn('/dev/ttyUSB0');
-        $this->dlt645 = new dlt645($this->conn);
     }
     public function start()
     {
+        $this->ttt();
+        $this->conn =  new conn('/dev/ttyUSB0');
+        $this->dlt645 = new dlt645($this->conn);
         // $dl3_04 = new di3_04($this->dlt645);
         // $dl3_04->setAddress("214790020322");
         $dl3_00 = new di3_00($this->dlt645);
         $dl3_00->setAddress("214790020322");
-        $energy = $dl3_00->getMeterEnergy(di3_00::DAYS[0], di3_00::RATES[0]);
+        $energy = $dl3_00->getMeterEnergy(di3_00::E_TYPE_DI2_A['组合有功'], di3_00::RATES_DI1['总电能'], di3_00::DAYS_DI0['当前']);
         $dl3_03 = new di3_03($this->dlt645);
         $dl3_00->setAddress("214790020322");
         $aVolt = $dl3_03->getAQuadrantVolt();
         $aElc = $dl3_03->getAQuadrantElc();
         $cPower = $dl3_03->getCurPower();
-        file_put_contents("/data/AxBuild/build/axDlt645/meter.log", '【' . date('Y-m-d H:i:s') . '】电能：' . $energy . '，电压：' . $aVolt . '，电流：' . $aElc . '，功率：' . $cPower . PHP_EOL, FILE_APPEND);
+        $this->conn->close();
     }
     public function ttt()
     {
-        // $str = '';
-        // for ($i = 0; $i < 64; $i++) {
-        //     $x = str_pad(strtoupper(dechex($i)), 2, '0', STR_PAD_LEFT);
-        //     $str .= '"' . $x . '",';
-        // }
-        // var_dump($str);
-        // exit;
+        $str = '';
+        for ($i = 0; $i < 64; $i++) {
+            $x = str_pad(strtoupper(dechex($i)), 2, '0', STR_PAD_LEFT);
+            $str .= '"费率' . $i . '电能"=>"' . $x . '",' . PHP_EOL;
+        }
+        print_r($str);
+        exit;
         // var_dump(hexdec("AA"));
         // exit;
     }
     public function __destruct()
     {
-        $this->conn->close();
     }
 }
